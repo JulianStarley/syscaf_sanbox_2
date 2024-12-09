11b936e6baa9fab564cdb696da844562f463658c
JulianStarley <julian2slg@gmail.com>
2024-12-09 14:20:19 -0400

    "terminando a conversao para x-blade-components"

--- resources/views/cadastros/novo_us_v2.blade.php
+++ resources/views/cadastros/novo_us_v2.blade.php
@@ -3,11 +3,15 @@
 
 {{-- Customize layout sections --}}
 @push('head')
+<!-- Bootstrap 5.3 -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<!-- Select2 CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet">
 <!-- Include jQuery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 @endpush
+
 @section('subtitle', 'Welcome')
 @section('content_header_title', 'Home')
 @section('content_header_subtitle', 'Welcome')
@@ -23,14 +27,12 @@
     <div class="card-body">
         <h2>Dados Básicos</h2>
         <hr>
-        <form>
+        <form id="novoUsuario">
+            <div class="form-group">
+                <x-adminlte-input name="iLabel" id="fullName" label="Nome completo" placeholder="nome completo sem abreviações" disable-feedback/>
+            </div>
         <div class="form-group">
-            <label for="fullName">Nome completo</label>
-            <input class="form-control" type="text" placeholder="Nome completo sem abreviações" id="fullName" name="fullName">
-        </div>
-        <div class="form-group">
-            <label for="socialName">Nome Social</label>
-            <input class="form-control" type="text" placeholder="Nome completo sem abreviações" id="socialName" name="socialName" disabled>
+            <x-adminlte-input name="iLabel" id="socialName" label="Nome social completo" placeholder="nome completo sem abreviações" disable-feedback />
             <div class="form-group">
                 <input class="form-check-input ml-1" type="checkbox" id="checkSocialName" name="checkSocialName">
                 <div>
@@ -39,8 +41,7 @@
             </div>
         </div>
         <div class="form-group">
-            <label for="userPhone">Telefones</label>
-            <input class="form-control" type="number" placeholder="Utilizar apenas números sem pontos (.) ou hifen (-)" id="userPhone" name="userPhone">
+            <x-adminlte-input name='iNum' id="phone" label="Telefone" placeholder="Insira seu(s) telefone(s) sem pontos (.) ou hifens (-)"/>
             <div class="form-group">
                 <input class="form-check-input ml-1" type="checkbox" id="checkEmergency" name="checkEmergency">
                 <div>
@@ -49,158 +50,109 @@
             </div>
         </div>
         <div class="form-group">
-            <label for="userCpf">CPF</label>
-            <input class="form-control" type="text" placeholder="use apenas números, sem pontos (.) ou hifen (-)" id="usercpf" name="userCpf">
+            <x-adminlte-input name="iLabel" id="CPF" label="Número do CPF" aria-placeholder="Insira os números sem pontos (.), espaços ou hifens(-)"/>
         </div>
             <div class="form-group">
-                <label>Nacionalidade</label>
-                <select class="form-control" id="nacionality" name="nacionality">
-                    <option>option 1</option>
-                    <option>option 2</option>
-                    <option>option 3</option>
-                    <option>option 4</option>
-                    <option>option 5</option>
-                </select>
+                <x-adminlte-select name="selBasic" label="Nacionalidade">
+                    <x-adminlte-options :options="['option 1', 'option 2','option 3', 'option 4', 'option 5']" empty-option="Selecione uma opção..." />
+                </x-adminlte-select>
             </div>
             <div class="form-group">
-                <label>Naturalidade</label>
-                <select class="form-control" id="naturalness" name="naturalness">
-                    <option>option 1</option>
-                    <option>option 2</option>
-                    <option>option 3</option>
-                    <option>option 4</option>
-                    <option>option 5</option>
-                </select>
+                <x-adminlte-select name="selBasic" label="Naturalidade">
+                    <x-adminlte-options :options="['option 1', 'option 2','option 3', 'option 4', 'option 5']"
+                        empty-option="Selecione uma opção..." />
+                </x-adminlte-select>
             </div>
         <h2>Dados Adicionais</h2>
         <hr>
             <div class="form-group">
-                <label>Gênero</label>
-                <select class="form-control" id="gender" name="gender">
-                    <option>option 1</option>
-                    <option>option 2</option>
-                    <option>option 3</option>
-                    <option>option 4</option>
-                    <option>option 5</option>
-                </select>
+                <x-adminlte-select name="selBasic" label="Gênero">
+                    <x-adminlte-options :options="['option 1', 'option 2','option 3', 'option 4', 'option 5']"
+                        empty-option="Selecione uma opção..." />
+                </x-adminlte-select>
             </div>
             <div class="form-group">
-                <label>Estado Cívil</label>
-                <select class="form-control" id="maritage" name="maritage">
-                    <option>option 1</option>
-                    <option>option 2</option>
-                    <option>option 3</option>
-                    <option>option 4</option>
-                    <option>option 5</option>
-                    <!-- /.card-body -->
-                </select>
+                <x-adminlte-select name="selBasic" label="Estado civil">
+                    <x-adminlte-options :options="['option 1', 'option 2','option 3', 'option 4', 'option 5']"
+                        empty-option="Selecione uma opção..." />
+                </x-adminlte-select>
             </div>
             <div class="form-group">
-                <label for="fullAddress">Endereço completo</label>
-                <input class="form-control" type="text" placeholder="Endereço completo do usuário" id="fullAddress" name="fullAddress">
+                <x-adminlte-input name="iLabel" id="fullAddress" label="Endereço completo" placeholder="Endereço completo" disable-feedback />
             </div>
             <div class="form-group">
-                <label>Observações</label>
-                <textarea class="form-control" rows="3" placeholder="Insira alguma observação adicional ou importante"></textarea>
+                <x-adminlte-textarea name="taBasic" placeholder="Inserir informações adicionais em detalhes" label="Observações"/>
             </div>
         <h2>Tipo de Usuário</h2>
         <hr>
-<div class="accordion" id="accordionFunction">
-    <div class="accordion-item">
-        <h2 class="accordion-header" id="headingOne">
+        </form>
+<!-- Accordion -->
+<div class="accordion row" id="accordionExample">
+    <div class="accordion-item col-md-4">
+        <h2 class="accordion-header">
             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
                 aria-expanded="true" aria-controls="collapseOne">
-                <strong>Funcionário</strong>
+                Funcionário
             </button>
         </h2>
-        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
-            data-bs-parent="#accordionFunction">
+        <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+               <div class="form-group">
+                    <label for="funcao">Função</label>
+                    <input class="form-control" type="text" placeholder="Insira a função que será exercida" id="funcao" name="funcao">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="accordion-item col-md-4">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Médico
+            </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
             <div class="accordion-body">
                 <div class="form-group">
-                    <label for="userFunction">Descrever função</label>
-                    <input class="form-control" type="text" placeholder="complete com sua função atual" id="userFunction"
-                        name="userFunction">
+                    <div class="form-group">
+                        <label for="crm">CRM</label>
+                        <input class="form-control" type="text" placeholder="Insira seu CRM" id="crm" name="crm">
+                    </div>
+                    <div class="form-group">
+                        <label>Especialidades</label>
+                        <select class="meuSelect " id="especialidades" name="especialidades[]" multiple>
+                            <option>Especialidade 1</option>
+                            <option>Especialidade 2</option>
+                            <option>Especialidade 3</option>
+                            <option>Especialidade 4</option>
+                            <option>Especialidade 5</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="accordion-item">
-        <h2 class="accordion-header" id="headingTwo">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                <strong>Médico</strong>
-            </button>
-        </h2>
-        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-            data-bs-parent="#accordionFunction">
-            <div class="accordion-body">
-                <div class="form-group">
-                    <label for="userDoc">CRM</label>
-                    <input class="form-control" type="text" placeholder="complete com seu crm ativo e principal" id="userDoc" name="userDoc">
-                </div>
-                <div class="input-group">
-                    <label for="meuSelect">Especialidades</label>
-                    <select id="meuSelect" name="meuSelect" multiple="true" class="form-control select2 select2-hidden-accessible" multiple="[]" style="width: 100%;" tabindex="-1" aria-hidden="true">
-                        <option value="1">Opção 1</option>
-                        <option value="2">Opção 2</option>
-                        <option value="3">Opção 3</option>
-                        <option value="4">Opção 4</option>
-                        <option value="5">Opção 5</option>
-                    </select>
-                <span class="select2 select2-container select2-container--default select2-container--below" dir="ltr" data-select2-id="8" style="width: 100%;">
-                    <span class="selection">
-                        <span class="select2-selection select2-selection--multiple" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="-1" aria-disabled="false">
-                            <ul class="select2-selection__rendered">
-                                <li class="select2-search select2-search--inline">
-                                    <input class="select2-search__field" type="search" tabindex="0" autocomplete="off" autocorrect="off" autocapitalize="none" spellcheck="false" role="searchbox" aria-autocomplete="list" placeholder="Select an option" style="width: 489.5px;">
-                                </li>
-                            </ul>
-                        </span>
-                    </span>
-                    <span class="dropdown-wrapper" aria-hidden="true">
-                    </span>
-                </span>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="accordion-item">
-        <h2 class="accordion-header" id="headingThree">
+        <h2 class="accordion-header">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                 data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                <strong>Farmacêutico</strong>
+                Farmacêutico
             </button>
         </h2>
-        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
-            data-bs-parent="#accordionFunction">
+        <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
             <div class="accordion-body">
-                <div class="form-group mt-2">
-                    <label for="userCRF">CRF</label>
-                    <input class="form-control" type="text" placeholder="CRF atual" id="userCRF" name="userCRF">
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="accordion-item">
-        <h2 class="accordion-header" id="headingFour">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                <strong>Paciente</strong>
-            </button>
-        </h2>
-        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionFunction">
-            <div class="accordion-body">
-                <div class="form-group mt-2">
- <label for="userSUS">Cartão SUS</label>
-                    <input class="form-control" type="number" placeholder="complete com seu cartão SUS" id="userSUS" name="userSUS">
-                </div>
+                    <div class="form-group">
+                        <label for="crf">CRF</label>
+                        <input class="form-control" type="text" placeholder="Insira seu CRF" id="crf" name="crf">
+                    </div>
             </div>
         </div>
     </div>
 </div>
-
         <div class="form-group">
             <button type="submit" class="btn btn-primary">Cadastrar</button>
         </div>
+    </form>
     </div>
 <!-- /.card -->
 </div>
@@ -216,45 +168,31 @@
 
 {{--
 <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-{{-- CSS select2 --}}
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<style>
-    .disabled-item {
-        background-color: #f0f0f0; /* Cor cinza */
+<!-- Bootstrap CSS -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
-    }
-    .active-item button {
-        background-color: #007bff; /* Cor azul */
-        color: white; /* Texto branco */
-    }
-</style>
 @endpush
 
 {{-- Push extra scripts --}}
 
 @push('js')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+<!-- Select2 JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+<!-- Bootstrap 5.3 js -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
 </script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
 $(document).ready(function() {
-
     //Initialize Select2 Elements
     $('.meuSelect').select2({
     theme: 'bootstrap4',
     closeOnSelect: false,
     placeholder: 'Selecione um item',
     multiple: true,
-    data: [
-        {id: 1, text: 'Item 1'},
-        {id: 2, text: 'Item 2'},
-        {id: 3, text: 'Item 3'},
-        {id: 4, text: 'Item 4'},
-        {id: 5, text: 'Item 5'},
-    ],
     })
 
     // Evento para habilitar/desabilitar o campo Nome Social
@@ -266,45 +204,6 @@ $(document).ready(function() {
     $('#socialName').prop('disabled', true); // Desabilita o campo
 }
 });
-});
-
-</script>
-<script>
-$(document).ready(function() {
-    $('.accordion-button').click(function() {
-        // Desabilitar todos os itens
-        $('.accordion-item').each(function() {
-            $(this).find('input, select').prop('disabled', true).addClass('disabled-item');
-        });
-
-        // Habilitar o item clicado
-        var target = $(this).data('bs-target');
-        $(target).find('input, select').prop('disabled', false).removeClass('disabled-item').addClass('active-item');
-
-        // Remover a classe ativa de todos os botões
-        $('.accordion-button').removeClass('active-item');
-        // Adicionar a classe ativa ao botão clicado
-        $(this).addClass('active-item');
-    });
-});
-</script>
-<script>
-$(document).ready(function() {
-    $('.accordion-button').click(function() {
-        // Desabilitar todos os itens
-        $('.accordion-item').each(function() {
-            $(this).find('input, select').prop('disabled', true).addClass('disabled-item');
-        });
-
-        // Habilitar o item clicado
-        var target = $(this).data('bs-target');
-        $(target).find('input, select').prop('disabled', false).removeClass('disabled-item').addClass('active-item');
-
-        // Remover a classe ativa de todos os botões
-        $('.accordion-button').removeClass('active-item');
-        // Adicionar a classe ativa ao botão clicado
-        $(this).addClass('active-item');
-    });
 });
 </script>
 @endpush
